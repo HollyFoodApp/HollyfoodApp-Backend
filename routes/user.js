fec7f83a39a50068eaa3b09aa494669c3d3242b8
Khairi Slimani <61380055+KhairiSlimani@users.noreply.github.com>
2023-03-22 04:15:24 +0100

    User Routes

---
+++ routes/user.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import { register, login, getAll, getById, updateOnce, deleteOnce } from '../controllers/user.js';
+import { body } from "express-validator";
+import multer from "../middlewares/multer-config.js";
+
+const router = express.Router();
+
+router
+    .route('/')
+    .post(
+        multer,
+        body("fullname").isLength({ min: 5 }),
+        body("fullname").isLength({ max: 30}),
+
+        body("email").isEmail(),
+
+        body("password").isLength({ min: 5 }),
+        body("password").isLength({ max: 30}),
+
+        body("phone").isNumeric(),
+        body("phone").isLength({ min: 8 }),
+        body("phone").isLength({ max: 8 }),
+
+        register
+    )
+    .patch(
+        body("fullname").isLength({ min: 5 }),
+        body("fullname").isLength({ max: 30}),
+
+        body("email").isEmail(),
+
+        body("password").isLength({ min: 5 }),
+        body("password").isLength({ max: 30}),
+
+        body("phone").isNumeric(),
+        body("phone").isLength({ min: 8 }),
+        body("phone").isLength({ max: 8 }),
+
+        updateOnce
+    )
+    .delete(deleteOnce)
+    .get(getAll)
+
+router
+    .route('/:id')
+    .get(getById)
+
+router
+    .route('/login')
+    .post(login)
+
+export default router;
